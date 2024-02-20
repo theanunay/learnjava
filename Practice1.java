04b2512f5082f8e5a96ab4907c410c37b76ce6d2
AnuNay SiNgh <65433948+theanunay@users.noreply.github.com>
2024-02-20 14:16:28 +0530

    Added Practice file for Operators

---
+++ Practice1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Practice1{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		// Q1. Rectangle Problem
+		
+		/* System.out.println("Enter the length: ");
+		int l = sc.nextInt();
+		
+		System.out.println("Enter the Breadth: ");
+		int b = sc.nextInt();
+		
+		int area = l*b;
+		int perimeter = 2*(l+b);
+		
+		System.out.println("Area of Rectangle is :" + area);
+		System.out.println("Perimeter of Rectangle is :" + perimeter); */
+		
+		
+// Q2. Traingle Problem 
+		/*
+		System.out.println("Enter the first side: ");
+		int a = sc.nextInt();
+		
+		System.out.println("Enter the second side: ");
+		int b = sc.nextInt();
+		
+		System.out.println("Enter the third side: ");
+		int c = sc.nextInt();
+		
+		int p = a+b+c;
+		
+		System.out.println("Perimeter of the Triangle: " + p);
+		*/
+		
+// Q3. BODMAS
+		/*
+		System.out.println("Enter the value: ");
+		int a = 8;
+		int b = 2345;
+		int c = 3;
+		int d = 5;
+		int e = 5;
+		
+		int result = (((a+b)/3)%5)*5;
+		
+		System.out.println("Formula: " + "(((a+b)/3)%5)*5");
+		System.out.println("Here is the result: " + result);
+		*/
+		
+		
+	}
+}
